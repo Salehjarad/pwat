65f9e03d0ebe0c7917f65e2ce3d0f3de01010d5c
salehjarad <salehjarad.art@gmail.com>
2025-07-30 09:33:04 +0300

    ficons

--- app/sal/page.tsx
+++ app/sal/page.tsx
@@ -1,7 +1,8 @@
 export default function SalPage() {
   return (
-    <div className="w-full h-full bg-blue-300 flex justify-center items-start sm:bg-green-300">
+    <div className="w-full h-full bg-green-300 flex justify-center items-start sm:bg-red-300">
       <p>Loading books...</p>
+      <h3>Yeeees</h3>
     </div>
   );
 }
