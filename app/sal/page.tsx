7835f0e0e23fd4c471f744d18677737bc7191854
salehjarad <salehjarad.art@gmail.com>
2025-07-30 09:17:03 +0300

    fcom

---
+++ app/sal/page.tsx
@@ -0,0 +1,7 @@
+export default function SalPage() {
+  return (
+    <div className="w-full h-full bg-blue-300 flex justify-center items-start sm:bg-green-300">
+      <p>Loading books...</p>
+    </div>
+  );
+}
